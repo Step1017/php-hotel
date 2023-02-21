c5fc57db4cc02614e9f40c75a976ef702f332e6f
Step1017 <s.aveta@yahoo.com>
2023-02-21 19:25:14 +0100

    Esercizio base completato

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     echo '<ul>';
     foreach ($hotels as $index => $hotel) {
         ?>
-            <li>
+            <!-- <li>
                 <?php /*var_dump($hotel);*/ ?>
                 <h3><?php echo ($index + 1).') '.$hotel['name']; ?></h3>
                 <p><?php echo $hotel["description"]; ?></p>
@@ -58,9 +58,71 @@
                         A <?php echo $hotel['distance_to_center']; ?> km dal centro
                     </span>
                 </div>
-            </li>
+            </li> -->
         <?php
     }
-    echo '</ul>';
+    //echo '</ul>';
+
+    // stampiamo come nomi delle colonne le chiavi dell'array
+    $tableHeaders = null;
+    if (count ($hotels) > 0) {
+        $tableHeaders = array_keys($hotels[0]);
+    }
+
+    // var_dump($tableHeaders);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>
+                            PHP HOTEL
+                        </h1>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <?php
+                                        if (isset($tableHeaders)) {
+                                            for ($i - 0; $i < count($tableHeaders); $i++) {
+                                                echo '<th scope="col">'.$tableHeaders[$i].'</th>';
+                                            }
+                                        }
+                                    ?>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    foreach ($hotels as $index => $hotel) {
+                                ?>
+                                    <tr>
+                                        <th scope="row"><?php echo ($index + 1); ?></th>
+                                        <td><?php echo $hotel["name"]; ?></td>
+                                        <td><?php echo $hotel["description"]; ?></td>
+                                        <td><?php echo ($hotel['parking'] ? 'Incluso' : 'NON incluso'); ?></td>
+                                        <td><?php echo $hotel["vote"]; ?> ★</td>
+                                        <td><?php echo $hotel["distance_to_center"]; ?> km</td>
+                                    </tr>
+                                <?php
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
 
